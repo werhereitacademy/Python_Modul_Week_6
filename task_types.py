bec26bff73b13beb193ac6cc967cc17a68cbfe5b
Faruk Furtana <94264525+ifafurt@users.noreply.github.com>
2025-01-24 18:03:08 +0100

    tesk_types

--- task_types.py
+++ task_types.py
@@ -1,5 +1,18 @@
 from task import Task
 
-# class PersonalTask(Task):
+class PersonalTask(Task):
+    def __init__(self, task_id, task_name, deadline):
+        super().__init__(task_id, task_name, deadline)
 
-# class WorkTask(Task):
+    def color_your_task(self): 
+        color = self.color = "blue" 
+        return color
+    
+class WorkTask(Task):
+    def __init__(self, task_id, task_name, deadline):
+        super().__init__(task_id, task_name, deadline)
+        self.priority = "high"
+        
+    def color_your_task(self):
+        color = self.color = "red"
+        return color
