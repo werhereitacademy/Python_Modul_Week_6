79b2e9ca51be743f37a961bdffd435c8e41e5c75
Mustafa HAYBAT <mustaphahaybat@gmail.com>
2025-05-21 13:08:19 +0200

    get_task_by_id Task_management dan alındı

--- Task Management System/task_tracking.py
+++ Task Management System/task_tracking.py
@@ -4,23 +4,17 @@ class TaskTracking:
     def __init__(self, task_management : TaskManagement):
         self.task_management = task_management
     
-    def get_task_by_id(self, task_id : int):
-        for task in self.task_management.task_list:
-            if task.task_id == task_id:
-                return task
-        return None
-    
     def get_task_status(self, task_id : int) -> None:
-        task = self.get_task_by_id(task_id)
+        task = self.task_management.get_task_by_id(task_id)
         if task:
-            print(f"Status of task {task_id}: {task.status}")
+            return task.status
     
     def get_task_deadline(self, task_id : int) -> None:
-        task = self.get_task_by_id(task_id)
+        task = self.task_management.get_task_by_id(task_id)
         if task:
-            print(f"Deadline of task {task_id}: {task.deadline}")
+            return task.deadline
 
     def get_task_color(self, task_id : int) -> None:
-        task = self.get_task_by_id(task_id)
+        task = self.task_management.get_task_by_id(task_id)
         if task:
-            print(f"Color of task {task_id}: {task.color}")
+            return task.color
