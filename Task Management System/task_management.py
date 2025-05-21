cededcd2a7b35656d9b4d883820337e6e2e9e601
FurkanBilal <furkanbilalcubuk@gmail.com>
2025-05-21 17:08:45 +0200

    Update task_management.py

--- Task Management System/task_management.py
+++ Task Management System/task_management.py
@@ -8,3 +8,7 @@ class TaskManagement:
     def display_tasks(self):
         for task in self.task_list:
             print(f"ID: {task.task_id}, Name: {task.task_name}, Deadline: {task.deadline}, Status: {task.status}, Priority: {task.priority}, Color: {task.color}")
+def get_task_by_id(self, task_id : int) -> None:
+        for task in self.task_list:
+            if task.task_id == task_id:
+                return task
