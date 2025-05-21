1042aa8502974e8bdb89f98350dfe1b9a65314c2
Ali Büyükgüral <alibuyukgural@gmail.com>
2025-05-21 08:52:29 +0200

    Update task.py

--- Task Management System/task.py
+++ Task Management System/task.py
@@ -1,12 +1,34 @@
 from abc import ABC, abstractmethod
 from datetime import datetime
 
-class Task(ABC):
-    pass
+class Task(ABC):         #soyut sinif
+    def __init__(self, task_id: int, task_name: str, deadline: str, status: str, priority: str):
+        self.task_id = task_id
+        self.task_name = task_name
+        self.deadline = deadline
+        self.status = status
+        self.priority = priority
+        self.color = self.color_your_task()
 
+
+    @abstractmethod
+    def color_your_task(self) -> str:
+        pass
+
+    def days_to_accomplish_task(self) -> int:
+        today = datetime.today()
+        try:
+            deadline_date = datetime.strptime(self.deadline, "%Y-%m-%d")
+            return (deadline_date - today).days
+        except ValueError:
+            return -1
+        
 
 class PersonalTask(Task):
-    pass
+    def color_your_task(self) -> str:
+        return "Blue"
+    
 
 class WorkTask(Task):
-    pass
+    def color_your_task(self) -> str:
+        return "Geel"
